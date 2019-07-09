22e568e751943dd4dd240492470d10a8385d1c3d
Alex Kingman <alex.kingman@blackbaud.com>
2019-07-09 16:35:37 -0400

    removed bad export

--- src/app/public/index.ts
+++ src/app/public/index.ts
@@ -3,6 +3,5 @@
 export * from './modules/split-view/split-view.module';
 
 // Split view types.
-export * from './modules/split-view/types/split-view-before-workspace-close-handler';
 export * from './modules/split-view/types/split-view-message';
 export * from './modules/split-view/types/split-view-message-type';
