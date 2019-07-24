02fb04579b4747d15f9f779639416ed2d2e53e28
BLACKBAUD\Trevor.Burch <Trevor.Burch@blackbaud.com>
2019-07-24 10:12:48 -0400

    Initial work to show overflow

---
+++ src/app/public/modules/split-view/split-view-workspace-content.component.ts
@@ -0,0 +1,13 @@
+import {
+  ChangeDetectionStrategy,
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'sky-split-view-workspace-content',
+  templateUrl: 'split-view-workspace-content.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SkySplitViewWorkspaceContentComponent {
+
+}
