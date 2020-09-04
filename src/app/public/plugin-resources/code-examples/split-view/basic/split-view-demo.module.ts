98e488ff293ccc6f86ea5b9ff22928a42b500484
Trevor Burch <Trevor.Burch@blackbaud.com>
2020-09-04 01:59:20 -0400

    Fix StackBlitz

--- src/app/public/plugin-resources/code-examples/split-view/basic/split-view-demo.module.ts
+++ src/app/public/plugin-resources/code-examples/split-view/basic/split-view-demo.module.ts
@@ -1,6 +1,30 @@
 import {
+  CommonModule
+} from '@angular/common';
+
+import {
   NgModule
 } from '@angular/core';
+
+import {
+  ReactiveFormsModule
+} from '@angular/forms';
+
+import {
+  SkySummaryActionBarModule
+} from '@skyux/action-bars';
+
+import {
+  SkyDefinitionListModule
+} from '@skyux/layout';
+
+import {
+  SkyRepeaterModule
+} from '@skyux/lists';
+
+import {
+  SkyConfirmModule
+} from '@skyux/modals';
 
 import {
   SkySplitViewModule
@@ -15,7 +39,13 @@ import {
     SkySplitViewDemoComponent
   ],
   imports: [
-    SkySplitViewModule
+    CommonModule,
+    ReactiveFormsModule,
+    SkyConfirmModule,
+    SkyDefinitionListModule,
+    SkyRepeaterModule,
+    SkySplitViewModule,
+    SkySummaryActionBarModule
   ],
   exports: [
     SkySplitViewDemoComponent
