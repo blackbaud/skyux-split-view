506dc3e40d1d8cbe84598d881ef6cab2c16854fe
Alex <alex.kingman@blackbaud.com>
2021-05-19 10:37:04 -0400

    Modern theme for split view component (#55)
    * Modern theme for split view component
    
    * Update src/app/public/modules/split-view/split-view-workspace.component.scss
    
    Co-authored-by: Blackbaud-ToddRoberts <todd.roberts@blackbaud.com>
    
    * PR feedback. fixed console error
    
    * focus style for slider
    
    * coverage
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>
    Co-authored-by: Blackbaud-ToddRoberts <todd.roberts@blackbaud.com>

--- e2e/split-view.e2e-spec.ts
+++ e2e/split-view.e2e-spec.ts
@@ -1,72 +1,84 @@
 import {
+  expect,
+  SkyHostBrowser,
+  SkyVisualThemeSelector
+} from '@skyux-sdk/e2e';
+
+import {
   by,
   element
 } from 'protractor';
 
-import {
-  expect,
-  SkyHostBrowser
-} from '@skyux-sdk/e2e';
-
 describe('Split view', () => {
-  beforeEach(() => {
-    SkyHostBrowser.get('visual/split-view');
-    SkyHostBrowser.setWindowBreakpoint('lg');
+  let currentTheme: string;
+  let currentThemeMode: string;
+
+  async function selectTheme(theme: string, mode: string): Promise<void> {
+    currentTheme = theme;
+    currentThemeMode = mode;
+
+    return SkyVisualThemeSelector.selectTheme(theme, mode);
+  }
+
+  function getScreenshotName(name: string): string {
+    if (currentTheme) {
+      name += '-' + currentTheme;
+    }
+
+    if (currentThemeMode) {
+      name += '-' + currentThemeMode;
+    }
+
+    return name;
+  }
+
+  function runTests() {
+    it('should match previous screenshot', async (done) => {
+      await SkyHostBrowser.scrollTo('#screenshot-split-view');
+      await SkyHostBrowser.setWindowBreakpoint('lg');
+      expect('#screenshot-split-view').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('split-view-lg')
+      });
+    });
+
+    it('should match previous screenshot (screen: xs)', async (done) => {
+      await SkyHostBrowser.scrollTo('#screenshot-split-view');
+      await SkyHostBrowser.setWindowBreakpoint('xs');
+      expect('#screenshot-split-view').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('split-view-xs')
+      });
+    });
+
+    it('should match previous screenshot when drawer is displayed in mobile view (screen: xs)', async (done) => {
+      await SkyHostBrowser.scrollTo('#screenshot-split-view');
+      await SkyHostBrowser.setWindowBreakpoint('xs');
+      element(
+        by.css('#screenshot-split-view .sky-split-view-workspace-header-content .sky-btn')
+      ).click();
+      await SkyHostBrowser.scrollTo('#screenshot-split-view');
+      expect('#screenshot-split-view').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('split-view-drawer-xs')
+      });
+    });
+  }
+
+  beforeEach(async () => {
+    await SkyHostBrowser.get('visual/split-view/basic');
+  });
+  runTests();
+
+  describe('when modern theme', () => {
+    beforeEach(async () => {
+      await selectTheme('modern', 'light');
+    });
+    runTests();
   });
 
-  it('should match previous screenshot', (done) => {
-    SkyHostBrowser.scrollTo('#screenshot-split-view');
-    expect('#screenshot-split-view').toMatchBaselineScreenshot(done, {
-      screenshotName: 'split-view-lg'
+  describe('when modern theme in dark mode', () => {
+    beforeEach(async () => {
+      await selectTheme('modern', 'dark');
     });
+    runTests();
   });
 
-  it('should match previous screenshot (screen: xs)', (done) => {
-    SkyHostBrowser.setWindowBreakpoint('xs');
-    SkyHostBrowser.scrollTo('#screenshot-split-view');
-    expect('#screenshot-split-view').toMatchBaselineScreenshot(done, {
-      screenshotName: 'split-view-xs'
-    });
-  });
-
-  // Extra test to show the responsive drawer.
-  // No large version is needed, as the "back" button doesn't exist on larger screens.
-  it('should match previous screenshot when drawer is displayed in mobile view (screen: xs)', (done) => {
-    SkyHostBrowser.setWindowBreakpoint('xs');
-    element(
-      by.css('#screenshot-split-view .sky-split-view-workspace-header-content .sky-btn')
-    ).click();
-    SkyHostBrowser.scrollTo('#screenshot-split-view');
-    expect('#screenshot-split-view').toMatchBaselineScreenshot(done, {
-      screenshotName: 'split-view-drawer-xs'
-    });
-  });
-
-  it('should match previous screenshot with repeater', (done) => {
-    SkyHostBrowser.scrollTo('#screenshot-split-view-with-repeater');
-    expect('#screenshot-split-view-with-repeater').toMatchBaselineScreenshot(done, {
-      screenshotName: 'split-view-repeater-lg'
-    });
-  });
-
-  it('should match previous screenshot with repeater (screen: xs)', (done) => {
-    SkyHostBrowser.setWindowBreakpoint('xs');
-    SkyHostBrowser.scrollTo('#screenshot-split-view-with-repeater');
-    expect('#screenshot-split-view-with-repeater').toMatchBaselineScreenshot(done, {
-      screenshotName: 'split-view-repeater-xs'
-    });
-  });
-
-  // Extra test to show the responsive drawer.
-  // No large version is needed, as the "back" button doesn't exist on larger screens.
-  it('should match previous screenshot when drawer is displayed in mobile view with repeater (screen: xs)', (done) => {
-    SkyHostBrowser.setWindowBreakpoint('xs');
-    element(
-      by.css('#screenshot-split-view-with-repeater .sky-split-view-workspace-header-content .sky-btn')
-    ).click();
-    SkyHostBrowser.scrollTo('#screenshot-split-view-with-repeater');
-    expect('#screenshot-split-view-with-repeater').toMatchBaselineScreenshot(done, {
-      screenshotName: 'split-view-drawer-repeater-xs'
-    });
-  });
 });
