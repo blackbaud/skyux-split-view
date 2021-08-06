633a78f38011c71545cfb1379cc135dd820e4035
John White <john.white@blackbaud.me>
2021-08-06 13:50:42 -0400

    Use real async for accessibility tests

--- src/app/public/modules/split-view/split-view.component.spec.ts
+++ src/app/public/modules/split-view/split-view.component.spec.ts
@@ -753,20 +753,20 @@ describe('Split view component', () => {
       });
     }));
 
-    it('should pass accessibility', async(() => {
+    it('should pass accessibility', async () => {
       fixture.componentInstance.ariaLabelForDrawer = 'My drawer';
       fixture.detectChanges();
-      fixture.whenStable();
+      await fixture.whenStable();
       return expectAsync(fixture.nativeElement).toBeAccessible();
-    }));
+    });
 
-    it('should pass accessibility when in responsive mode', async(() => {
+    it('should pass accessibility when in responsive mode', async () => {
       initiateResponsiveMode(fixture);
-      fixture.whenStable();
+      await fixture.whenStable();
       return expectAsync(fixture.nativeElement).toBeAccessible();
-    }));
+    });
 
-    it('should pass accessibility when scrolling', async(() => {
+    it('should pass accessibility when scrolling', async () => {
       component.ariaLabelForDrawer = 'My drawer';
       Array.from(Array(70).keys()).forEach((i) => {
         component.items.push({
@@ -776,8 +776,8 @@ describe('Split view component', () => {
         component.additionalItems.push(`additional item ${i + 1}`);
       });
       fixture.detectChanges();
-      fixture.whenStable();
+      await fixture.whenStable();
       return expectAsync(fixture.nativeElement).toBeAccessible();
-    }));
+    });
   });
 });
